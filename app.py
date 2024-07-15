23ec8850e96452f6f818a38f29f1ed4ee0ef9709
msmubassir <125449220+msmubassir@users.noreply.github.com>
2024-07-15 16:26:16 +0600

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def user_upload():
     return render_template('user_upload.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))

977901ceea05e35b39868fe4c3efeef72d75319c
msmubassir <125449220+msmubassir@users.noreply.github.com>
2024-07-15 17:01:16 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 from flask import Flask, request, render_template, redirect, url_for
-import os
-from utils import save_image
+from utils import save_image, is_image_safe
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/admin_upload', methods=['GET', 'POST'])
 def admin_upload():
     if request.method == 'POST':
         image = request.files['image']
-        label = request.form['label']
-        if image and label in ['nude', 'not_nude']:
+        label = request.form.get('label')  # Assuming a form input for label (e.g., 'nude', 'not_nude')
+        if image:
             try:
-                filepath = save_image(image, label, app.config['UPLOAD_FOLDER'])
-                # Additional processing or model training can go here
-                return redirect(url_for('admin_upload'))
+                save_image(image, label, app.config['UPLOAD_FOLDER'])
+                return "File uploaded successfully."
             except Exception as e:
                 print(f"Error saving image: {e}")
                 return "Error processing upload", 500
@@ -27,8 +29,16 @@ def user_upload():
         if image:
             try:
                 filepath = save_image(image, None, app.config['UPLOAD_FOLDER'])
-                # Perform prediction or processing here
-                return "File uploaded successfully"
+                
+                # Check if the uploaded image is safe (not 'nude')
+                safe = is_image_safe(filepath)
+                
+                if safe:
+                    result_message = "It's not nude."
+                else:
+                    result_message = "It's nude."
+                
+                return render_template('user_upload.html', message=result_message)
             except Exception as e:
                 print(f"Error saving image: {e}")
                 return "Error processing upload", 500
